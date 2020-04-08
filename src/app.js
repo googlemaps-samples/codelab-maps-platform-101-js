1ebb64cf17ee70cec090808cc34bbaf7ee789cf0
Alex Muramoto <amuramoto@apache.org>
2020-04-08 11:28:08 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,101 @@
+import MarkerClusterer from '@google/markerclustererplus';
+
+const googleMapsAPIKey = 'YOUR API KEY';
+loadMapsJSAPI(googleMapsAPIKey);
+
+function runApp() {
+  const map = initMap();
+  const markers = addMarkers(map);
+  const markerCluster = clusterMarkers(map, markers);
+  addPanToMarker(map, markers);
+  drawCircle(map);
+}
+
+function loadMapsJSAPI(googleMapsAPIKey) {
+
+  const googleMapsAPIURI = `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPIKey}&callback=runApp`,
+    head = document.querySelector('head'),
+    script = document.createElement('script');
+
+  window['runApp'] = runApp;
+
+  script.type = 'text/javascript';
+  script.src = googleMapsAPIURI;
+  script.defer = true;
+  script.async = true;
+  head.appendChild(script);
+}
+
+function initMap() {
+  let center = { lat: -33.860664, lng: 151.208138 };
+  let zoom = 14;
+  let mapDiv = document.getElementById('map');
+  let map = new google.maps.Map(mapDiv, {
+    center: center,
+    zoom: zoom
+  });
+  return map;
+}
+
+function addMarkers(map) {
+  const locations = {
+    operaHouse: { lat: -33.8567844, lng: 151.213108 },
+    tarongaZoo: { lat: -33.8472767, lng: 151.2188164 },
+    manlyBeach: { lat: -33.8209738, lng: 151.2563253 },
+    hyderPark: { lat: -33.8690081, lng: 151.2052393 },
+    theRocks: { lat: -33.8587568, lng: 151.2058246 },
+    circularQuay: { lat: -33.858761, lng: 151.2055688 },
+    harbourBridge: { lat: -33.852228, lng: 151.2038374 },
+    kingsCross: { lat: -33.8737375, lng: 151.222569 },
+    botanicGardens: { lat: -33.864167, lng: 151.216387 },
+    museumOfSydney: { lat: -33.8636005, lng: 151.2092542 },
+    maritimeMuseum: { lat: -33.869395, lng: 151.198648 },
+    kingStreetWharf: { lat: -33.8665445, lng: 151.1989808 },
+    aquarium: { lat: -33.869627, lng: 151.202146 },
+    darlingHarbour: { lat: -33.87488, lng: 151.1987113 },
+    barangaroo: { lat: - 33.8605523, lng: 151.1972205 }
+  }
+  const markers = [];
+  for (const location in locations) {
+    const markerOptions = {
+      position: locations[location],
+      icon: './img/custom_pin.png'
+    }
+    const marker = new google.maps.Marker(markerOptions);
+    markers.push(marker);
+  }
+  return markers;
+}
+
+function clusterMarkers(map, markers) {
+  const options = { imagePath: './img/m' }
+  const markerCluster = new MarkerClusterer(map, markers, options);
+  return markerCluster;
+}
+
+function addPanToMarker(map, markers) {
+  let circle;
+  markers = markers.map(marker => {
+    marker.addListener('click', event => {
+      const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
+      map.panTo(location);
+      if (circle) {
+        circle.setMap(null);
+      }
+      circle = drawCircle(map, location);
+    });
+  });
+  return markers;
+}
+
+function drawCircle(map, location) {
+  const circle = new google.maps.Circle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 1,
+    map: map,
+    center: location,
+    radius: 800
+  });
+  return circle;
+}
